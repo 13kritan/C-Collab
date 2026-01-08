d47ddd10fb76b103a1de9f0d058e8c142ed52753
13kritan <13kritan@gmail.com>
2026-01-08 14:08:46 +0545

    nav dropdown

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import { Search, Plus } from 'lucide-react';
+import React, { useRef, useState } from 'react'
+import { Search, Plus } from 'lucide-react'
+import { User, Settings, Terminal, Shield } from 'lucide-react'
+import ProfileDropdown from './Dropdown';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const dropdownRef = useRef(null)
+
+  const menuItems = [
+    { label: 'Profile', icon: <User size={14} />, detail: '0xAF31...' },
+    { label: 'Settings', icon: <Settings size={14} /> },
+    { label: 'API Keys', icon: <Terminal size={14} /> },
+    { label: 'Security', icon: <Shield size={14} /> },
+  ]
   return (
     <nav className="fixed z-20 top-0 left-0 pl-72 w-full h-16 border-b border-white/[0.05] bg-bg-subtle flex items-center justify-between px-8">
       {/* Left Section: Search Bar */}
@@ -21,19 +32,24 @@ const Navbar = () => {
       {/* Right Section: Actions & Profile */}
       <div className="flex items-center gap-4">
         {/* New Project Button */}
-        <button className="flex items-center gap-2 bg-accent-blue hover:bg-blue-600 text-white text-xs font-bold py-2 px-4 rounded-md transition-all shadow-[0_0_15px_rgba(59,130,246,0.3)] active:scale-95 cursor-pointer">
+        <button className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-slate-100 text-xs font-bold py-2 px-4 rounded-md transition-all shadow-[0_0_15px_rgba(59,130,246,0.3)] active:scale-95 cursor-pointer">
           <Plus size={16} strokeWidth={3} />
           <span>New Project</span>
         </button>
 
         {/* User Profile */}
         <div className="relative flex items-center gap-2 pl-2 border-l border-white/[0.1]">
-          <div className="h-8 w-8 rounded-full overflow-hidden border border-accent-blue shadow-[0_0_8px_rgba(59,130,246,0.5)] cursor-pointer hover:opacity-80 transition-opacity">
-            <img 
+          <div onClick={() => setIsOpen(!isOpen)}
+          ref={dropdownRef} 
+          className="h-8 w-8 rounded-full overflow-hidden border border-accent-blue shadow-[0_0_8px_rgba(59,130,246,0.5)] cursor-pointer">
+            <img
               src="/api/placeholder/32/32" // Replace with your avatar source
               alt="User"
               className="h-full w-full object-cover"
             />
+            {
+              isOpen && <ProfileDropdown menuItems={menuItems} dropdownRef={dropdownRef} isOpen={isOpen} setIsOpen={setIsOpen} />
+            }
           </div>
         </div>
       </div>
