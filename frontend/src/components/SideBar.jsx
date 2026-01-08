99d18e65823b8994834c9ce96a3c21e19a70c4d1
13kritan <13kritan@gmail.com>
2026-01-08 14:09:22 +0545

    menu update

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import { LayoutGrid, Box, Share2, Activity } from 'lucide-react'; // Optional icons
+import React, { useState } from 'react'
+import { LayoutGrid, Box, Share2, Activity } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const Sidebar = () => {
-  const [activeTab, setActiveTab] = useState('Projects');
+  const [activeTab, setActiveTab] = useState('Projects')
+  const navigate = useNavigate()
 
   const menuItems = [
-    { name: 'Projects', icon: <LayoutGrid size={18} /> },
-    { name: 'Structs', icon: <Box size={18} /> },
-    { name: 'Shared', icon: <Share2 size={18} /> },
-    { name: 'Activity', icon: <Activity size={18} /> },
-  ];
+    { name: 'Projects', icon: <LayoutGrid size={18} />, path: 'home' },
+    { name: 'Recent', icon: <Box size={18} />, path: 'recent' },
+    { name: 'Shared', icon: <Share2 size={18} />, path: 'shared' },
+    { name: 'Activity', icon: <Activity size={18} />, path: 'activity' },
+  ]
 
   return (
     <div className="fixed z-30 top-0 left-0 w-64 h-screen bg-bg-main border-r border-white/[0.05] flex flex-col py-6 ">
@@ -20,7 +22,7 @@ const Sidebar = () => {
           <span className="px-0.5">C</span>
           <span className="opacity-50">]</span>
         </div>
-        <h1 className='font-mono font-bold text-3xl tracking-tighter text-slate-200'>- Collab</h1>
+        <h1 className='font-mono font-bold text-2xl tracking-tighter text-slate-200'>- Collab</h1>
       </div>
 
       {/* Navigation Links */}
@@ -28,7 +30,10 @@ const Sidebar = () => {
         {menuItems.map((item) => (
           <button
             key={item.name}
-            onClick={() => setActiveTab(item.name)}
+            onClick={() => {
+              setActiveTab(item.name)
+              navigate(`/${item.path}`)
+            }}
             className={`
               w-full flex items-center gap-4 py-3 px-8 transition-all duration-200
               font-mono text-sm group relative
@@ -51,7 +56,7 @@ const Sidebar = () => {
         ))}
       </nav>
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
